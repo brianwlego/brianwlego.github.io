e6ae5dbb872cbd90fab31eae4927b6508362ab85
brianwlego <legobw@gmail.com>
2020-10-29 12:32:57 -0400

    Adding CNAME file

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Intro from './components/Intro'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Intro />
     </div>
   );
 }
---
+++ portfolio/src/components/Intro.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Intro() {
+
+
+  return (
+    <section className="section intro">
+      <h3>Hi!</h3>
+      <h3>I'm Brian Lego, a full-stack web developer.  </h3>
+    </section>
+  )
+}
+
+export default Intro
