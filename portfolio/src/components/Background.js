b41e13929bf2df04e0bded8dee37b847bea9c6d3
brianwlego <legobw@gmail.com>
2020-11-20 17:23:32 -0500

    Changes to CSS

--- portfolio/src/components/Background.js
+++ portfolio/src/components/Background.js
@@ -9,9 +9,9 @@ function Background(){
         <h3>Background</h3>
       </div>
       <div id="background-content">
-        <p>A recent graduate from the <span className="bold">Flatiron School</span> in NYC with a concentration in Software Engineering. A few years before that I attended <span className="bold">Florida State University</span> for a Bachelor of Fine Arts degree in musical theatre. After FSU I was fortunate enough to work on cruise ships around the world performing for thousands.</p>
+        <p>A recent graduate from the <span className="bold">Flatiron School</span> in NYC with a concentration in Software Engineering. A few years before that I attended <span className="bold">Florida State University</span> for a Bachelor of Fine Arts degree in musical theatre. After FSU, I was fortunate enough to work on cruise ships around the world performing for thousands.</p>
 
-        <p> More recently I've been lucky enough to channel all my passion into building cool things on the web! I love working within teams and collaborating to create useful and innovative products for users.</p>
+        <p> More recently I've been channelling all my passion into building cool things on the web! I love working within teams and collaborating to create innovative products with great a user interface.</p>
           
         <p>When I'm not clacking away on my laptop you can find me playing tennis &#127934; or watching reruns of Top Chef  &#128104;&#8205;&#127859;</p>
       </div>
