25e30e1c40154e7cae11bacccc684836c3ea6efe
brianwlego <legobw@gmail.com>
2020-12-14 10:57:49 -0500

    adding fetch calls

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -7,8 +7,27 @@ import NavBar from './components/NavBar';
 import Projects from './components/Projects';
 
 function App() {
+  const dummyFetches = () => {
+    fetch('https://quizcard-frontend.herokuapp.com/')
+    .then(console.log).catch(console.log)
+    fetch('https://quizcard-backend.herokuapp.com/')
+    .then(console.log).catch(console.log)
+
+    fetch('https://notfcebook-backend.herokuapp.com/')
+    .then(console.log).catch(console.log)
+    fetch('https://notfcebook.herokuapp.com/')
+    .then(console.log).catch(console.log)
+
+    fetch('https://cocktail-companion.herokuapp.com/')
+    .then(console.log).catch(console.log)
+    fetch('https://cocktailcompanion-api.herokuapp.com/')
+    .then(console.log).catch(console.log)
+  }
+
+
   return (
     <div className="App">
+      {dummyFetches()}
       <NavBar />
       <Intro />
       <Background />
